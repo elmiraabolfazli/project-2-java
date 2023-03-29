51dc57034d1f5185c108cb9ff0a84b67d4fb28ab
elmira abolfazli <efabolfazli@gmail.com>
2023-03-29 10:19:10 +0330

    2

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,7 +2,22 @@ public class Main {
     public static void main(String[] args) {
         Users users=new Users();
         while (true){
-            users.menu1();
+           int n= users.menu1();
+           switch (n){
+               case 1:
+                   n=users.singIn();
+                   switch (n){
+                       case 1:
+                           break;
+                       case 2:
+                           break;
+                   }
+                   break;
+               case 2:
+                  users.singUp();
+                   break;
+           }
+
 
         }
     }
--- untitled/src/Users.java
+++ untitled/src/Users.java
@@ -1,10 +1,52 @@
 import java.util.Scanner;
 
 public class Users {
-    int menu1(){
+    private User[] usersArry= new User[100];
+    private int numberOfUser=0;
+    Scanner input=new Scanner(System.in);
+    public int menu1(){
         System.out.println("1-Sing in");
         System.out.println("2-Sing up");
-        Scanner input=new Scanner(System.in);
        return input.nextInt();
     }
+    public void singUp(){
+        usersArry[numberOfUser]=new User();
+        System.out.println("Enter your username");
+        String name=input.nextLine();
+        name=input.nextLine();
+        usersArry[numberOfUser].setUsername(name);
+        System.out.println("Enter your password");
+        String password=input.nextLine();
+        usersArry[numberOfUser].setPassword(password);
+        ++numberOfUser;
+    }
+    public int singIn(){
+        int n=0;
+        System.out.println("Enter your username");
+        String name=input.nextLine();
+        name=input.nextLine();
+
+        if(name.equals("Admin")){
+
+        }else{
+            for (int i = 0; i < numberOfUser; i++) {
+                if(usersArry[i].getUsername().equals(name)){
+                    System.out.println("Enter your password");
+                    String password=input.nextLine();
+                    if(usersArry[i].getPassword().equals(password)){ n=1;}
+                     else {
+                        System.out.println("Wrong password!");
+                        n=0;
+                    }
+                }else{
+                    System.out.println("Wrong username !");
+                    n=0;
+                }
+
+            }
+        }
+        System.out.println(n);
+        return n;
+    }
+
 }
